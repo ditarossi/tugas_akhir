a79431748036eaeb7ea9d451c3f39579e3b44aef
Dita Rossiyana <ditarossiyana12@gmail.com>
2022-03-29 13:18:33 +0700

    third post

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view('/template', 'template.v_template');
+
+Route::view('/admin', 'admin.dashboard');
+
+Route::view('/tabel', 'admin.user');
+
+Route::view('/home', 'home.home');
